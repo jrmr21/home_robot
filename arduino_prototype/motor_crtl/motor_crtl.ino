540cee3c872ebdaa2c87ca49692de7497b92a272
jeremie.robles@mainbot.me <jeremierobles@gmx.fr>
2020-05-03 01:16:15 +0200

    first version connectde

--- arduino_prototype/motor_crtl/motor_crtl.ino
+++ arduino_prototype/motor_crtl/motor_crtl.ino
@@ -5,7 +5,7 @@
 void setup() 
 {
   uint8_t   prepare   = 1;
-  uint16_t  time_out  = 500;
+  uint16_t  time_out  = 0;
   
   init_pinOut();
   driver_init();
@@ -23,20 +23,26 @@ void setup()
    
   while (prepare)
   {
-    String    data;
-    
+    uint8_t    data[4];
+
+    Serial.println("wait card...");
     if (uart.available() > 0)
     {
-      data = uart.readString();
+      uart.readBytes(data, 4);
 
-      if (data[0] == 48)
+      if (data[0] == 0)
       {
         time_out |= data[1] << 8;
         time_out |= data[2];
 
         set_min_power(data[3]);
-        
+
         Serial.println("succes setup");
+        Serial.print("time max : ");
+        Serial.println(time_out, DEC);
+
+        Serial.print("power min: ");
+        Serial.println(data[3], DEC);
         prepare = 0;
       }
 
@@ -46,18 +52,53 @@ void setup()
   
   while (1)
   {
-    String    data;
-    uint8_t i   = 0;
-    
-    data[0] = 0;
+    uint8_t    data[2] = {0};
     
     if (uart.available() > 0)
     {
-      data = uart.readString();
+      uart.readBytes(data, 2);
 
-      Serial.print("I received: ");
-      Serial.println(data);
+      Serial.println(data[0], DEC);
+      Serial.println(data[1], DEC);
+      
+      switch (data[0])
+      {
+        case 15 :
+          Serial.println("change min speed");
+          set_min_power(data[1]);
+          break;
+        
+        case 16 :
+          Serial.println("foward move");
+          motor_G_crtl(1);
+          motor_D_crtl(1); 
+          break;
+          
+        case 17 :
+          Serial.println("backward move");
+          motor_G_crtl(2);
+          motor_D_crtl(2);
+          break;
+        
+        case 18 :
+          Serial.println("left move");
+          motor_G_crtl(2);
+          motor_D_crtl(1);
+          break;
 
+        case 19 :
+          Serial.println("right move");
+          motor_G_crtl(1);
+          motor_D_crtl(2);
+          break;
+          
+        case 20 :
+          Serial.println("STOP !! move");
+          motor_G_crtl();
+          motor_D_crtl(); 
+          break;  
+      }
+      
       uart.flush();
     }
   }
